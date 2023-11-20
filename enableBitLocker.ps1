6ff6811a4ebcb333b293a747e983a93e17e00ce0
ljjSpectrum <151569348+ljjSpectrum@users.noreply.github.com>
2023-11-20 14:51:41 -0600

    Update enableBitLocker.ps1

--- enableBitLocker.ps1
+++ enableBitLocker.ps1
@@ -5,20 +5,20 @@ $driveLetter = "C:"
 $bitlockerStatus = Get-BitLockerVolume -MountPoint $driveLetter | Select-Object -ExpandProperty EncryptionPercentage -ErrorAction SilentlyContinue
 
 if ($bitlockerStatus -eq $null) {
-    # BitLocker is not enabled, so enable it
-    Enable-BitLocker -MountPoint $driveLetter -EncryptionMethod XtsAes256
 
     # Add BitLocker recovery key protector to store in Active Directory
     $recoveryKeyProtector = Add-BitLockerKeyProtector -MountPoint $driveLetter -RecoveryPasswordProtector
-
+    
+    # Backup key to AD
+    BackupToAAD-BitLockerKeyProtector -MountPoint "C:" -KeyProtectorId $recoveryKeyProtector.KeyProtector[1].KeyProtectorId
+    
     # Display the recovery key
     $recoveryKey = $recoveryKeyProtector.RecoveryPassword
     Write-Host "BitLocker has been enabled on drive $($driveLetter)."
     Write-Host "Recovery Key: $recoveryKey"
-
-    # You may want to backup the recovery key to a secure location in addition to Active Directory
-    # Backup-BitLockerKeyProtector -MountPoint $driveLetter -KeyProtectorId $recoveryKeyProtector.KeyProtectorId
-    # Replace the above line with the appropriate command for your backup strategy
+  
+    # BitLocker is not enabled, so enable it
+    Enable-BitLocker -MountPoint $driveLetter -EncryptionMethod XtsAes256
 } else {
     Write-Host "BitLocker is already enabled on drive $($driveLetter)."
 }
